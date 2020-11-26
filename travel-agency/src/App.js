07a7851c457e7a2f521faea47a23473a571db9e7
LeoLuisBispo <46049421+LeoLuisBispo@users.noreply.github.com>
2020-11-26 16:27:43 +0100

    commmit

--- travel-agency/src/App.js
+++ travel-agency/src/App.js
@@ -1,15 +1,20 @@
-import { ApiStorage } from "./ApiContext";
+import React from 'react'
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
 import PhotoPrincipal from "./components/Navbar/PhotoPrincipal";
 import CardMaps from "./components/Maps/CardMaps";
 import AgenteCard from "./components/Agente/AgenteCard";
+import { ApiStorage, ApiContext } from "./ApiContext";
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <NavBar/>
+     
       <ApiStorage>
+        <NavBar/>
         <PhotoPrincipal />
         <CardMaps />
         <AgenteCard />
