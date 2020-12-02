c22a6d9135c40add0b564b8e0cf805d901b3ebaf
violafederico <fedev93@gmail.com>
2020-12-02 10:22:44 +0100

    login form

--- travel-agency/src/components/Login/Login.js
+++ travel-agency/src/components/Login/Login.js
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 import logo from '../../img/logo.svg';
 import { Link } from 'react-router-dom';
 
-function Login() {
+function Login({mychanges=(()=>{})}) {
+
+    const [mail, setMail] = useState('');
+    const [password, setpassword] = useState('');
+
+    function handleChange(event){  
+        const name = event.target.name;
+        const value = event.target.value;
+        if(name === 'mail'){ setMail({ mail:value})}
+        if(name === 'password'){ setpassword({ password:value})}
+        
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("event ", mail, " ", password)
+        /** creare auth to provider **/
+    }
 
     return (
         <>
             <div className="loginContainer">
                 <div className="imgLogin">
-                    <img src={logo} />
+                    <img src={logo} alt="logo"/>
                 </div>
                 <div className="loginformdiv">
-                    <form className="loginform" noValidate>
+                    <form onSubmit={handleSubmit} noValidate className="loginform" >
                         <label>Email</label>
-                        <input type="email" placeholder="Email..." />
+                        <input type="email"
+                                name='mail' 
+                                placeholder="Email..."
+                                onChange={handleChange}
+                        />
                         <label>Password</label>
-                        <input type="password" placeholder="Password..." />
+                        <input type="password"
+                                name='password' 
+                                placeholder="Password..." 
+                                onChange={handleChange}/>
                     </form>
-                    <button type="submit" className='btnLogin'><Link to='/homepage'> Login</Link> </button>
+                    <button type="submit" className='btnLogin' onClick={handleSubmit}> 
+                    {/* <Link to='/homepage'> Login</Link> */}
+                    Login
+                    </button>
                 </div>
             </div>
         </>
