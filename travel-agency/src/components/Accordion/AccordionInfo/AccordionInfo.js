f6b9ab1897f4c9afc86c62f676998b839f5013d0
violafederico <fedev93@gmail.com>
2020-12-04 12:23:44 +0100

    accordion style

--- travel-agency/src/components/Accordion/AccordionInfo/AccordionInfo.js
+++ travel-agency/src/components/Accordion/AccordionInfo/AccordionInfo.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./AccordionInfoStyles.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faAngleDoubleDown} from "@fortawesome/free-solid-svg-icons";
 
 const AccordionInfo = ({ children, title, isExpand = false }) => {
+
+  const angle = <FontAwesomeIcon icon={faAngleDoubleDown} />;
 
   const [expand, setExpand] = React.useState(isExpand);
   
@@ -9,7 +13,7 @@ const AccordionInfo = ({ children, title, isExpand = false }) => {
     <div className="box">
       <div className="title-box" onClick={() => setExpand(expand => !expand)}>
         <span className="title">{title}</span>
-        <span className="icon"><i className={`fas fa-angle-down${!expand ? ' down' : ''}`}></i></span>
+        <span className="icon"><span className="icona">{angle}</span><i className={`${!expand ? ' down' : ''}`}></i></span>
         <div className="clearfix"></div>
       </div>
       {expand && <div className="content">{children}</div>}
